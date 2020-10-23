9d565026fed612946598f0565693876e1e56c794
Samarit <kislyakaleksey@gmail.com>
2020-10-23 18:56:58 +0300

    Server initialized, created Mongo DB, User auth model created

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+const PORT = config.get('port')
+
+const app = express()
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURI', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }))
+        app.listen(PORT, () => {console.log(`App has been started on port ${PORT}`);})
+    } catch(e) {
+        console.log('Server Error: ', e.message)
+        process.exit(1)
+    }
+}
+
---
+++ routes/auth.routes.js
@@ -0,0 +1,82 @@
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const User = require('../models/User')
+const router = Router()
+
+// /api/auth/register
+router.post(
+    '/register',
+    [
+        check('email', 'Email is incorrect').isEmail(),
+        check('password', 'Password is incorrect').isLength({min: 6})
+    ],
+    async (req, res) => {
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty) {
+            return req.status(400).json({
+                message: 'Data input is incorrect!',
+                errors: errors.array()
+            })
+        }
+
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({message: 'This user is already exist!'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email: email, password: hashedPassword})
+
+        await user.save()
+
+        res.status(200).json({message: 'User created'})
+        
+    } catch(e) {
+        res.status(500).json({message: 'Something went wrong with server, try again'})
+    }
+})
+
+// /api/auth/login
+router.post('/login',
+    [
+        check('email', 'Email is incorrect').normalizeEmail().isEmail(),
+        check('password', 'Password is incorrect').exists().isLength({min: 6})
+    ],
+    async (req, res) => {
+    try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty) {
+            return req.status(400).json({
+                message: 'Data input is incorrect!',
+                errors: errors.array()
+            })
+        }
+
+        const {email, password} = req.body
+
+        const user = await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({message: 'User not found'})
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({message: 'Wrong password, try again'})
+        }
+
+    } catch(e) {
+        res.status(500).json({message: 'Something went wrong with server, try again'})
+    }
+})
+
+
+module.exports = router
