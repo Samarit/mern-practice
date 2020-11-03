5d2b2fbeb129e0c3001713e12a295ced3d313a13
Samarit <kislyakaleksey@gmail.com>
2020-11-03 21:58:25 +0300

    Application finished!

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const config = require('config')
 const mongoose = require('mongoose')
 const PORT = config.get('port')
@@ -12,6 +13,14 @@ app.use(cors())
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 async function start() {
